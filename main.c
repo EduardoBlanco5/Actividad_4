a39edc3f6ac4e535cb2fffb10323a2f288542a3c
Eduardo Blanco <eduardo.blanco0841@alumnos.udg.mx>
2020-09-21 21:03:52 -0500

    Funciona el ingreso de un numero

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int n;
+    printf("Hola a todos \n");
+    printf("ingresa un numero: ");
+    scanf("%i", &n);
+    printf("Hola numero %i", n);
+    
+    return 0;
+
+}
